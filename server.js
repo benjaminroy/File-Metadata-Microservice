4bf60f31bce77a3412e4ac3ff23897317a3aeb62
roy_benjamin <benji01560@gmail.com>
2017-01-23 04:07:27 +0000

    Added multer package + minor changes

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-var express = require('express')
-var app = express()
+var express = require('express');
 var path = require("path");
+var multer = require('multer');
+var port = 8080;
+var app = express();
+
+var storage =   multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+        
+    },
+    filename: function (req, file, callback) {
+        callback(null,Date.now() + file.originalname);
+        
+    }
+});
+var upload = multer({ storage : storage}).single('myfile.txt');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/bower_components')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
-})
+});
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.post('/upload', function (req, res) {
+    upload(req, res, function(err) {
+        if (err) {
+            console.log("Error uploading file.");
+            return res.end("Error uploading file.");
+            
+        }
+        console.log("File is uploaded");
+        console.log(req.files);
+        res.end("File is uploaded");
+    });    
+});
+
+
+app.listen(port, function () {
+    console.log('App listening on port ' + port.toString() + '...');
+});
